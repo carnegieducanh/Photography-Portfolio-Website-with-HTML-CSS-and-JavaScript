50f1280c9b6d97c53896b774f55a8cf7fdf4bde7
ducanhhuy <huyducanh@gmail.com>
2026-09-08 11:06:44 +0900

    Done!

--- main.js
+++ main.js
@@ -39,3 +39,38 @@ ScrollReveal().reveal(".header__btn", {
   ...scrollRevealOption,
   delay: 1500,
 });
+
+ScrollReveal().reveal(".about__image img", {
+  ...scrollRevealOption,
+  origin: "left",
+});
+
+ScrollReveal().reveal(".about__content .section__header", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".about__content p", {
+  ...scrollRevealOption,
+  delay: 1000,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".about__btn", {
+  ...scrollRevealOption,
+  delay: 2000,
+});
+
+ScrollReveal().reveal(".blog__card", {
+  duration: 1000,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".blog__btn", {
+  ...scrollRevealOption,
+  delay: 2000,
+});
+
+ScrollReveal().reveal(".contact__image img", {
+  ...scrollRevealOption,
+});
